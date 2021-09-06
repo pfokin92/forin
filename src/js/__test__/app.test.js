c3d60a56c1e54b8d1a5500df201676eacdc1fca2
Pavel Fokin <fokinpavel2007@outlook.com>
2021-09-06 23:22:49 +0300

    First commit

---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,24 @@
+﻿import orderByProps from '../app'
+
+test('for..in',()=>{
+    const obj = {
+        name: 'мечник', 
+        health: 10, 
+        level: 2, 
+        attack: 80, 
+        defence: 40
+    }
+
+
+    const result = [
+        {key: "name", value: "мечник"}, 
+        {key: "level", value: 2}, 
+        {key: "attack", value: 80}, 
+        {key: "defence", value: 40},
+        {key: "health", value: 10} 
+      ];
+
+    
+    expect(orderByProps(obj,["name", "level"])).toStrictEqual(result);
+
+});
---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+﻿export default function orderByProps(obj, order) {
+    const keysArr = [];
+    const sortArr = [];
+
+    for(let key in obj){
+        if(order.includes(key)){
+            keysArr.push({
+              key,
+              value: obj[key]
+            });
+
+        }
+        else{
+            sortArr.push({
+              key,
+              value: obj[key]
+            });
+
+        }
+    }
+    sortArr.sort((a, b) => {
+        if(a.key>b.key){
+            return 1;
+        }
+        if(a.key<b.key){
+            return -1;
+        }
+        return 0;
+    });
+    return [...keysArr,...sortArr];   
+    
+}
