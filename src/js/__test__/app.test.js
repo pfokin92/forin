f783e715993ebbffb6d48132a99edca90996c429
Pavel Fokin <fokinpavel2007@outlook.com>
2021-10-01 22:55:57 +0300

    fixed edlint

--- src/js/__test__/app.test.js
+++ src/js/__test__/app.test.js
@@ -1,24 +1,21 @@
-﻿import orderByProps from '../app'
+import orderByProps from '../app';
 
-test('for..in',()=>{
-    const obj = {
-        name: 'мечник', 
-        health: 10, 
-        level: 2, 
-        attack: 80, 
-        defence: 40
-    }
+test('for..in', () => {
+  const obj = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
 
+  const result = [
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ];
 
-    const result = [
-        {key: "name", value: "мечник"}, 
-        {key: "level", value: 2}, 
-        {key: "attack", value: 80}, 
-        {key: "defence", value: 40},
-        {key: "health", value: 10} 
-      ];
-
-    
-    expect(orderByProps(obj,["name", "level"])).toStrictEqual(result);
-
+  expect(orderByProps(obj, ['name', 'level'])).toStrictEqual(result);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,29 @@
-﻿export default function orderByProps(obj, order) {
-    const keysArr = [];
-    const sortArr = [];
+export default function orderByProps(obj, order) {
+  const keysArr = [];
+  const sortArr = [];
 
-    for(let key in obj){
-        if(order.includes(key)){
-            keysArr.push({
-              key,
-              value: obj[key]
-            });
-
-        }
-        else{
-            sortArr.push({
-              key,
-              value: obj[key]
-            });
-
-        }
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key in obj) {
+    if (order.includes(key)) {
+      keysArr.push({
+        key,
+        value: obj[key],
+      });
+    } else {
+      sortArr.push({
+        key,
+        value: obj[key],
+      });
     }
-    sortArr.sort((a, b) => {
-        if(a.key>b.key){
-            return 1;
-        }
-        if(a.key<b.key){
-            return -1;
-        }
-        return 0;
-    });
-    return [...keysArr,...sortArr];   
-    
+  }
+  sortArr.sort((a, b) => {
+    if (a.key > b.key) {
+      return 1;
+    }
+    if (a.key < b.key) {
+      return -1;
+    }
+    return 0;
+  });
+  return [...keysArr, ...sortArr];
 }
